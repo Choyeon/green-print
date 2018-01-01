0544efa9074ed113365fff22470c3957a48e67ae
雨过初晴 <admin@choyeon.cn>
2018-01-01 09:35:00 +0800

    test_github_streak

--- green.py
+++ green.py
@@ -23,10 +23,10 @@ def commit():
 
 def set_sys_time(month, day, year):
     print('sudo date %02d%02d1200%04d' % (month, day, year))
-    os.system('sudo date %02d%02d1200%04d' % (month, day, year))
+    os.system('sudo date %02d%02d0935%04d' % (month, day, year))
 
 
-def trick_commit(year, month, day):
+def trick_commit(month, day, year):
     set_sys_time(month, day, year)
     modify()
     commit()
@@ -39,4 +39,4 @@ def daily_commit(start_date, end_date):
 
 
 if __name__ == '__main__':
-    daily_commit(datetime.date(2009, 1, 1), datetime.date(2019, 12, 30))
+    daily_commit(datetime.date(2018, 1, 1), datetime.date(2020, 5, 20))
